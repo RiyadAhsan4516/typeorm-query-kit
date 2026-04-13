fe6e5cde363f6a05dc24fa9bcc77438853fe6ec4
riyad <riyad.ahsan4516@gmail.com>
2026-04-13 15:18:31 +0600

    - upgraded version, facilitates nest pipe as well

--- dist/queryParserPipe.d.ts
+++ dist/queryParserPipe.d.ts
@@ -1,31 +1,25 @@
-declare namespace QueryParserTypes {
-    interface QuerySort {
-        field: string;
-        order: 'ASC' | 'DESC';
-    }
-    interface QueryRange {
-        [field: string]: {
-            gt?: number;
-            gte?: number;
-            lt?: number;
-            lte?: number;
+export declare class QueryParserPipe {
+    parse(query: any): {
+        filter: {
+            [field: string]: any;
         };
-    }
-    interface QueryFilter {
-        [field: string]: any;
-    }
-    interface QueryOptions {
-        filter: QueryFilter;
-        range: QueryRange;
-        sort?: QuerySort[];
+        range: {
+            [field: string]: {
+                gt?: number;
+                gte?: number;
+                lt?: number;
+                lte?: number;
+            };
+        };
+        sort?: {
+            field: string;
+            order: 'ASC' | 'DESC';
+        }[];
         page: number;
         limit: number;
         relations: string[];
         config?: any;
-    }
-}
-export declare class QueryParserPipe {
-    parse(query: any): QueryParserTypes.QueryOptions;
+    };
     private parseFilter;
     private validateFilter;
     private parseRange;
@@ -35,4 +29,3 @@ export declare class QueryParserPipe {
     private parseLimit;
     private parseRelations;
 }
-export {};
